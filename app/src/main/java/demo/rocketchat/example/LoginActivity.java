4ddc9562f786e49e9af760a176a7b7570fb98453
sachin <sachinshinde7676@gmail.com>
2017-08-13 23:42:54 +0530

    added members to loginActivity

--- app/src/main/java/demo/rocketchat/example/LoginActivity.java
+++ app/src/main/java/demo/rocketchat/example/LoginActivity.java
@@ -2,12 +2,22 @@ package demo.rocketchat.example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatEditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    AppCompatEditText username;
+    AppCompatEditText password;
+    AppCompatButton login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        username = (AppCompatEditText) findViewById(R.id.username);
+        password = (AppCompatEditText) findViewById(R.id.password);
+        login = (AppCompatButton) findViewById(R.id.password);
+
     }
 }
